9348dd042ae57ea66ff1a96191d32bdf51605132
EmmN <emanuel.nedelcu@gmail.com>
2016-02-29 00:13:54 +0200

    try to create a story based on email params

--- app/controllers/sendgrid_webhooks_controller.rb
+++ app/controllers/sendgrid_webhooks_controller.rb
@@ -1,6 +1,7 @@
 class SendgridWebhooksController < ActionController::Base
   def create
-    project = Project.where(:sendgrid_email => params[:to]).first
+    email_regex = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i
+    project     = Project.where(:sendgrid_email => params[:to]).first
 
     if project.present?
       tracker_project = Tracker::Project.from_project(project)
@@ -8,7 +9,25 @@ class SendgridWebhooksController < ActionController::Base
       description = params[:text] if description.blank?
       description = ReverseMarkdown.convert(description)
 
-      # story = tracker_project.stories.create(:story_type => 'bug', :name => params[:subject], :description => description )
+      
+      requester_email = (params[:from] || "").scan(email_regex).to_a.first || "emanuel@mytime.com"
+      requester = tracker_project.project.memberships.all.find{|e| e.email == requester_email}
+
+      new_story_attrs = {
+        :story_type      => 'bug', 
+        :name            => params[:subject], 
+        :description     => description,
+        :estimate        => 0,
+        :requested_by_id => requester.try(:id)
+      }
+      if params[:cc].present?
+        owner_email = params[:cc].scan(email_regex).to_a.first
+        owner = tracker_project.project.memberships.all.find{|e| e.email == owner_email}
+
+        new_story_attrs[:owner_ids] = [owner.try(:id)]
+      end
+      
+      story = tracker_project.stories.create(new_story_attrs)
     end
 
     render :json => { "message" => "OK" }, :status => :ok
