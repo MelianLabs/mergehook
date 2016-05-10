8609b3b5bb9e7ed216af7bc19d17ceed34e70b66
EmmN <emanuel.nedelcu@gmail.com>
2016-05-11 00:05:04 +0300

    build on circleci when label is LGTM / Needs Review

--- app/services/pull_request_labeler.rb
+++ app/services/pull_request_labeler.rb
@@ -3,7 +3,16 @@ require "octokit"
 class PullRequestLabeler < PullRequestActionBase
   def run
     require "pp"
-    options = {:branch => @payload[:pull_request][:head][:ref]}
-    pp "PullRequestLabeler", options, @project
+    options = {
+      :branch => @payload[:pull_request][:head][:ref]
+    }
+
+    res = nil
+    if ["LGTM", "Needs Review"].include?(@payload[:label][:name])
+      res = CircleciBuilder.new(options, @project).run
+      puts "@res >>>", @res
+    end
+        
+    res
   end
 end
