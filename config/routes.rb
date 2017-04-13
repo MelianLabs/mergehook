c1158125cab22101a886885a6c47cad3a0db04cc
Emanuel Nedelcu <emanuel.nedelcu@gmail.com>
2017-04-13 15:27:25 +0300

    try to allow .

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :projects do
     member do
-      get "trigger_build/:branch", :to => :trigger_build, :format => false
+      get "trigger_build/:branch", :to => :trigger_build, :format => false, :constraints => { :branch => /[^\/]+/ }
     end
   end
 
